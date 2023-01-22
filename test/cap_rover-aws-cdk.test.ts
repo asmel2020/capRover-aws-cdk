0feb578ce1c0500fbfabb75f643328d148ca0125
Danny Gonzalez <danny.galicia@rhizom.me>
2023-01-22 07:21:31 -0300

    Initial commit

---
+++ test/cap_rover-aws-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CapRoverAwsCdk from '../lib/cap_rover-aws-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cap_rover-aws-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CapRoverAwsCdk.CapRoverAwsCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
